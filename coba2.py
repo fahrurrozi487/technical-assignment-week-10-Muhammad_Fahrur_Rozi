23aacfaee4894eb56dbdbd71f795e32d38c91453
fahrurrozi487 <138858583+fahrurrozi487@users.noreply.github.com>
2023-08-19 14:31:34 +0700

    Add files via upload

---
+++ coba2.py
@@ -0,0 +1,57 @@
+import time
+import requests
+import math
+import random
+
+TOKEN = "BBFF-aIEftEV2daGk0dOU5eIhSXPXwPErD5"
+DEVICE_LABEL = "TIM_12" 
+VARIABLE_LABEL_1 = "ultrasonic_sensor"
+
+
+def build_payload(variable_1):
+    value_1 = random.randint(-10, 50)
+    lat = random.randrange(34, 36, 1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    lng = random.randrange(-83, -87, -1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    payload = {variable_1: value_1,
+                }
+
+    return payload
+
+
+def post_request(payload):
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(VARIABLE_LABEL_1)
+
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
